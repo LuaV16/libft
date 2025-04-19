67bcdcf7430a5b3d7976196aa2dc00aa209ac94b
LuaV16 <lua.vargas.ulloa@gmail.com>
2025-04-19 16:44:54 +0200

    Add more list functions

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: lvargas- <lvargas-@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/08 15:47:19 by lvargas-          #+#    #+#             */
-/*   Updated: 2025/04/18 22:44:54 by lvargas-         ###   ########.fr       */
+/*   Updated: 2025/04/19 16:36:53 by lvargas-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -59,5 +59,9 @@ void				ft_putendl_fd(char *s, int fd);
 void				ft_putnbr_fd(int n, int fd);
 t_list				*ft_lstnew(void *content);
 void				ft_lstadd_front(t_list **lst, t_list *new);
+int					ft_lstsize(t_list *lst);
+t_list				*ft_lstlast(t_list *lst);
+void				ft_lstadd_back(t_list **lst, t_list *new);
+void				ft_lstdelone(t_list *lst, void (*del)(void *));
 
 #endif
